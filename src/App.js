2903878f407259fcc9de48b5f06f39c7b906e09d
Lori Gildersleeve <lgildersleeve@gmail.com>
2023-04-04 16:47:22 -0400

    Clean up base theme colors.

--- src/App.js
+++ src/App.js
@@ -7,15 +7,44 @@ import '@fontsource/material-icons'
 
 import { ThemeProvider, createTheme } from '@mui/material/styles'
 import CssBaseline from '@mui/material/CssBaseline'
-import './App.css'
 
 const baseTheme = createTheme({
+  breakpoints: {
+    values: {
+      mobile: 0,
+      tablet: 640,
+      laptop: 1024,
+      desktop: 1200,
+    },
+  },
   palette: {
-    mode: 'dark',
+    mode: 'light',
+    primary: {
+      main: '#005CB6',
+    },
+    secondary: {
+      main: '#E34025',
+    },
+    warning: {
+      main: '#FFC709',
+      contrastText: '#ffffff',
+    },
+    error: {
+      main: '#FF671B',
+      contrastText: '#ffffff',
+    },
+    divider: '#262626',
+    info: {
+      main: '#005CB6',
+    },
+  },
+  typography: {
+    fontFamily: 'Lato, Roboto, Arial',
+    fontSize: '10'
   },
 })
 
-export default function App({ title = 'Webpack/Babel React with MUI, and Storybook' }) {
+export default function App({ title }) {
   return (
     <ThemeProvider theme={baseTheme}>
       <CssBaseline>
--- src/index.js
+++ src/index.js
@@ -3,6 +3,6 @@ import ReactDOM from 'react-dom'
 
 import App from './App'
 
-const title = 'React with Webpack and Babel'
+const title = 'Webpack/Babel React with MUI and Storybook'
 
 ReactDOM.render(<App title={title} />, document.getElementById('app'))
